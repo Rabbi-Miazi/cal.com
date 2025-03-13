b5ef3b8917c9dee3a847078cf3a9816aa6ce4c9d
Rabbi-Miazi <rabbymiazi333@gmail.com>
2025-03-13 16:09:36 +0600

    Update constants.ts

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -78,7 +78,7 @@ export const PUBLIC_QUICK_AVAILABILITY_ROLLOUT =
 
 /** @deprecated use `WEBAPP_URL` */
 export const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_WEBAPP_URL || `https://${process.env.VERCEL_URL}`;
-export const LOGO = "/calcom-logo-white-word.svg";
+export const LOGO = "/tunelailogo.svg";
 export const LOGO_ICON = "/tunelailogo.svg";
 export const AVATAR_FALLBACK = "/avatar.svg";
 export const FAVICON_16 = "/favicon-16x16.png";
